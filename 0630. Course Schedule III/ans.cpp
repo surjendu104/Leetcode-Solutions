adaf27586793b8365eae1750b07c663582259ea9
surjendu104 <cyber00daddy@gmail.com>
2022-06-23 11:04:43 +0530

    23.06.2022

---
+++ 0630. Course Schedule III/ans.cpp
@@ -0,0 +1,23 @@
+/*Runtime: 338 ms, faster than 58.36% of C++ online submissions for Course Schedule III.
+Memory Usage: 56 MB, less than 97.05% of C++ online submissions for Course Schedule III.*/
+
+class Solution
+{
+public:
+    int scheduleCourse(vector<vector<int>> &C)
+    {
+        sort(C.begin(), C.end(), [](auto &a, auto &b)
+             { return a[1] < b[1]; });
+        priority_queue<int> pq;
+        int total = 0;
+        for (auto &course : C)
+        {
+            int dur = course[0], end = course[1];
+            if (dur + total <= end)
+                total += dur, pq.push(dur);
+            else if (pq.size() && pq.top() > dur)
+                total += dur - pq.top(), pq.pop(), pq.push(dur);
+        }
+        return pq.size();
+    }
+};
