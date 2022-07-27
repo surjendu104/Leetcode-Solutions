4df26e3415e320778391cc7eff4e3e8ef036192c
surjendu104 <cyber00daddy@gmail.com>
2022-07-27 08:38:04 +0530

    27.07.2022

---
+++ 0114. Flatten Binary Tree to Linked List/ans.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+
+/*
+ * Runtime: 0 ms, faster than 100.00% of Java online submissions for Flatten
+ * Binary Tree to Linked List.
+ * Memory Usage: 42.8 MB, less than 36.07% of Java online submissions for
+ * Flatten Binary Tree to Linked List.
+ */
+class Solution {
+    TreeNode prev = null;
+
+    public void flatten(TreeNode root) {
+        if (root == null)
+            return;
+        flatten(root.right);
+        flatten(root.left);
+        root.right = prev;
+        root.left = null;
+        prev = root;
+    }
+}
