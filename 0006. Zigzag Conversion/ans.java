8acb346e1b1b6b194f42b0e59521dce0f4d7ff31
surjendu104 <cyber00daddy@gmail.com>
2022-09-10 18:44:38 +0530

    10.09.2022

--- 0006. Zigzag Conversion/ans.java
+++ 0006. Zigzag Conversion/ans.java
@@ -2,26 +2,26 @@ import java.util.*;
 
 class Solution {
     public String convert(String s, int numRows) {
-        int n=s.length();
-        int i=0;
+        int n = s.length();
+        int i = 0;
 
         // List<String> ans = new ArrayList<String>(numRows);
         String[] ans = new String[numRows];
-        
+
         Arrays.fill(ans, "");
-        while(i<n){
-            for(int j=0;j<numRows && i<n;j++){
-                ans[j]+=Character.toString(s.charAt(i));
+        while (i < n) {
+            for (int j = 0; j < numRows && i < n; j++) {
+                ans[j] += Character.toString(s.charAt(i));
                 i++;
             }
-            for(int j=numRows-2;j>=1 && i<n;j--){
-                ans[j]+=Character.toString(s.charAt(i));
+            for (int j = numRows - 2; j >= 1 && i < n; j--) {
+                ans[j] += Character.toString(s.charAt(i));
                 i++;
             }
         }
         String res = "";
-        for(String x:ans){
-            res+=x;
+        for (String x : ans) {
+            res += x;
         }
         return res;
     }
