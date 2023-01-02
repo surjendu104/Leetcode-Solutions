22f2617256eda951920187171e6e98e461f4c6ef
surjendu104 <cyber00daddy@gmail.com>
2023-01-02 10:14:45 +0530

    02.02.2023

---
+++ 0521. Longest Uncommon Subsequence I/ans.cpp
@@ -0,0 +1,10 @@
+class Solution
+{
+public:
+    int findLUSlength(string a, string b)
+    {
+        if (a == b)
+            return -1;
+        return a.size() > b.size() ? a.size() : b.size();
+    }
+};
