9f85ccbc324f609f6ac091c66566a0d4d27c1694
Surjendu <101238933+surjendu104@users.noreply.github.com>
2023-05-12 13:45:21 +0530

    Time: 78 ms (24.48%), Space: 43 MB (81.84%) - LeetHub

---
+++ 2629-function-composition/2629-function-composition.js
@@ -0,0 +1,21 @@
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+var compose = function (functions) {
+  return function (x) {
+    if (functions.length === 0) return x;
+    let input = x;
+
+    for (const func of functions.reverse()) {
+      input = func(input);
+    }
+
+    return input;
+  };
+};
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
