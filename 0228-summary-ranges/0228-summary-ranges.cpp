c3b092a2a2fa5e4a80066aea676093f15829fad4
Surjendu <101238933+surjendu104@users.noreply.github.com>
2023-06-12 17:01:08 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (13.50%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.cpp
+++ 0228-summary-ranges/0228-summary-ranges.cpp
@@ -3,19 +3,19 @@ public:
     vector<string> summaryRanges(vector<int>& nums) {
         int n = nums.size();
         vector<string> tmp;
-        if(n==1) {
-            tmp.push_back(to_string(nums[0]));
-            return tmp;
-        }
-        if(n==2) {
-            if(nums[0]-nums[1] == -1)tmp.push_back(to_string(nums[0])+"->"+to_string(nums[1]));
-            else {
-                tmp.push_back(to_string(nums[0]));
-                tmp.push_back(to_string(nums[1]));
-            }
-            return tmp;
+//         if(n==1) {
+//             tmp.push_back(to_string(nums[0]));
+//             return tmp;
+//         }
+//         if(n==2) {
+//             if(nums[0]-nums[1] == -1)tmp.push_back(to_string(nums[0])+"->"+to_string(nums[1]));
+//             else {
+//                 tmp.push_back(to_string(nums[0]));
+//                 tmp.push_back(to_string(nums[1]));
+//             }
+//             return tmp;
             
-        }
+//         }
         
         
         
@@ -28,14 +28,14 @@ public:
             
             if(i!=j)s+= "->"+to_string(nums[j]);
             // cout<<"dbg : "<<i<<' '<<j<<' '<<s<<endl;
-            i = j+1;
             j++;
+            i = j;
             tmp.push_back(s);
             s = "";
         }
         
-        for(string t : tmp)cout<<t<<' ';
-        cout<<endl;
+        // for(string t : tmp)cout<<t<<' ';
+        // cout<<endl;
         
         return tmp;
     }
