43982e8629b5d9065c2fcd5339f5d707bbf184ef
Surjendu <101238933+surjendu104@users.noreply.github.com>
2023-07-06 11:28:15 +0530

    Time: 1 ms (100.00%), Space: 56.2 MB (7.83%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
@@ -4,23 +4,27 @@ class Solution {
         int len = Integer.MAX_VALUE, sum = 0;
         int i = 0;
         
+        // If sum of array elements are less than target then return 0 because no subarray
+        // will be exists that satisfy the condition
         int check = 0;
-        for(int x : nums)check+=x;
+        for(int x : nums)check += x;
         if(check < target)return 0;
         
         while(j < n) {
+            // Go to the index while sum less than target
             while(sum < target && j < n) {
                 sum += nums[j];
                 j++;
             }
+            // find the minimum
             len = Math.min(len, j-i+1);
-            System.out.println("dbg1 : "+len);
+            // now when sum greater than target then shrink the window and check for minimum window
             while(sum >= target) {
                 sum-=nums[i];
                 i++;
             }
+            // get the minimum satisfying window
             len = Math.min(len, j-i+1);
-            System.out.println("dbg1 : "+len);
         }
         return len;
     }
