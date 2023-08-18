fa2d9621006405454269c396bcb027342a4e436c
Surjendu <101238933+surjendu104@users.noreply.github.com>
2023-08-18 09:38:50 +0530

    Update 0300-longest-increasing-subsequence.java
    Added another approach

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -17,4 +17,21 @@ class Solution {
 
         return curr[0];
     }
+    /*
+    // dp[i] :: length of LIS upto index i
+    int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, 1);
+        int len = Integer.MIN_VALUE;
+        
+        for(int i = 0; i < n; ++i) {
+            for(int j = 0; j < i; ++j) {
+                if(nums[j] < nums[i])dp[i] = Math.max(dp[i], 1+dp[j]);
+            }
+            len = Math.max(len, dp[i]);
+        }
+        return len;
+    }
+    */
 }
