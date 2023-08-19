9dfa38973b2acc0195034459d1b3b702d3a590c6
Surjendu <101238933+surjendu104@users.noreply.github.com>
2023-08-19 12:25:47 +0530

    Added solution - LeetHub

---
+++ Matrix Chain Multiplication - GFG/matrix-chain-multiplication.java
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while(t-- > 0){
+            int N = Integer.parseInt(in.readLine());
+            String input_line[] = in.readLine().trim().split("\\s+");
+            int arr[]= new int[N];
+            for(int i = 0; i < N; i++)
+                arr[i] = Integer.parseInt(input_line[i]);
+            
+            Solution ob = new Solution();
+            System.out.println(ob.matrixMultiplication(N, arr));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    static int solve(int i, int j, int arr[], int[][] dp) {
+        if(i == j) return 0;
+        if(dp[i][j] != -1) return dp[i][j];
+        int mini = Integer.MAX_VALUE;
+        
+        for(int k = i; k < j; ++k) {
+            int steps = (arr[i-1] * arr[k] * arr[j]) 
+                        + solve(i, k, arr, dp) + solve(k+1, j, arr, dp);
+            mini = Math.min(mini, steps);
+        }
+        return dp[i][j] = mini;
+    }
+    static int matrixMultiplication(int N, int arr[])
+    {
+        int[][] dp = new int[N+1][N+1];
+        for(int[] it : dp) Arrays.fill(it, -1);
+        return solve(1, N-1, arr, dp);
+    }
+}
